5663e76f4832ef96bbfbc0ca16d1d002f950fea7
RaghuRaman <98797101+raghu-raman-btech@users.noreply.github.com>
2024-11-16 00:15:50 +0530

    Update SSSConfig.kt

--- src/main/kotlin/com/msp/backHub/config/SSSConfig.kt
+++ src/main/kotlin/com/msp/backHub/config/SSSConfig.kt
@@ -11,8 +11,8 @@ import org.springframework.context.annotation.Configuration
 class SSSConfig {
 
     companion object {
-        private const val ACCESS_KEY = "AKIAQE43KCP74PA2TYSB"
-        private const val SECRET_KEY = "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"
+        private const val ACCESS_KEY = "xxx"
+        private const val SECRET_KEY = "yyy"
         private const val REGION = "ap-southeast-2"
     }
 
